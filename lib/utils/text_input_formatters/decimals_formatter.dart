04162a5e305fa00804bc2bbcc35f60884f72e6d4
Aleš Tichava <d4rkw34v3r@gmail.com>
2022-12-04 11:50:21 +0100

    save

--- lib/utils/text_input_formatters/decimals_formatter.dart
+++ lib/utils/text_input_formatters/decimals_formatter.dart
@@ -4,17 +4,12 @@ class DecimalsFormatter extends TextInputFormatter {
   static final _doubleNumberRegExp = RegExp(r'^(0|[1-9][0-9]*)[,.]{0,1}[0-9]*$');
   static final _noDigitsButDecimals = RegExp(r'^\.[0-9]+$');
   static final _leadingZeroesRegExp = RegExp(r'^00+\.?[0-9]*$');
-  static final _zeroesRegExp = RegExp(r'^\.?00+$');
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
     String newFieldValue = newValue.text.replaceAll(',', '.');
     if (newFieldValue.isEmpty) {
-      return const TextEditingValue(text: '');
-    }
-
-    if (_zeroesRegExp.hasMatch(newFieldValue)) {
-      return const TextEditingValue(text: '0', selection: TextSelection.collapsed(offset: 1));
+      return newValue;
     }
 
     if (_leadingZeroesRegExp.hasMatch(newFieldValue)) {
